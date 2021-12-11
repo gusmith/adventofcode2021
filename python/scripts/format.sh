b085ab327a15867b6d617bf7dd5092ccaad9b443
Guillaume Smith <gwil1248@fastmail.net>
2021-12-11 15:30:23 +1100

    Day 9 done!

--- python/scripts/format.sh
+++ python/scripts/format.sh
@@ -6,4 +6,5 @@ module="src"
 autoflake --remove-duplicate-keys --remove-unused-variables --remove-all-unused-imports -i -r $module tests
 seed-isort-config --application-directories $module &&
 isort $module tests
+docformatter --in-place -r $module
 black $module tests
