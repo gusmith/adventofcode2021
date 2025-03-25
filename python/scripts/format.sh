2fa94a2412a4cc6c53b462c332bc64199c5671cb
Guillaume Smith <gwil1248@fastmail.net>
2025-03-25 21:31:41 +1100

    Update most dependencies, and use ruff as formatter and linter

--- python/scripts/format.sh
+++ python/scripts/format.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -e
 set +x
 
-module="src"
-
-autoflake --remove-duplicate-keys --remove-unused-variables --remove-all-unused-imports -i -r $module tests
-seed-isort-config --application-directories $module &&
-isort $module tests
-docformatter --in-place -r $module
-black $module tests
+ruff format
